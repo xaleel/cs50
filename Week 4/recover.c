90e777a594a2c68a3c6521ea809f239e8fcd1378
xaleel <98000000+xaleel@users.noreply.github.com>
2022-02-15 21:21:49 +0300

    Update recover.c

--- Week 4/recover.c
+++ Week 4/recover.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <cs50.h>
 
 typedef uint8_t BYTE;
 
@@ -15,26 +14,32 @@ int main(int argc, char *argv[])
     FILE *f = fopen(argv[1], "r");
     BYTE buffer[512];
     int num = 0;//number of output JPEGs
-    FILE *img = fopen ("temp.jpg", "r");//to initialize *img to be able to close it before we create the first jpg
-    while (fread(&buffer, 1, 512, f) == 512 && img != NULL)
+    FILE *img = NULL;//to initialize *img to be able to close it before we create the first jpg
+    while (fread(&buffer, 1, 512, f) == 512)// && img != NULL
     {
         char filename [8];
         if (buffer[0] == 255 && buffer [1] == 216 && buffer [2] == 255 && (buffer[3] & 240) == 224) //if start of a new JPEG
         {
-            fclose(img);//close previous file
-            sprintf(filename, "%03i.jpg", num);//make new filename
-            img = fopen (filename, "a");//make new jpg with new filename
-            fwrite (&buffer, 1, 512, img);//copy the block in buffer to newfilenamejpg
+            if (num == 0)//if 1st jpg
+            {
+                sprintf(filename, "%03i.jpg", num); //make new filename
+                img = fopen(filename, "w"); //open filename.jpg
+            }
+            else
+            {
+                fclose(img);
+                sprintf(filename, "%03i.jpg", num); //make new filename
+                img = fopen(filename, "w"); //open filename.jpg
+            }
+            fwrite(&buffer, 1, 512, img); //copy the block in buffer to newfilenamejpg
             num ++;//increase no. of jpgs
         }
         else
         {
-            if (fopen (filename, "a") != NULL)//if you can open the jpg
+            if (num > 0)//don't run before the 1st jpg is found
             {
-                img = fopen (filename, "a");//open filename.jpg
-                fwrite (&buffer, 1, 512, img);//copy next block
+                fwrite(&buffer, 1, 512, img); //copy the next block in buffer to newfilenamejpg
             }
         }
     }
-
 }
